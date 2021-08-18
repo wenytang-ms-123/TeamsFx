b23b4042b1bc6dc81c9641817bef0fdce713f675
Tian Yuan <tiany1992@sina.com>
2021-08-18 10:23:05 +0800

    fix: add fake pass

---
+++ packages/cli/src/commonlib/testPass.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+"use strict";
+
+export const password = "123456";
+export const password2 = "-jrE4_iVxYX6AjTQu-N01b_6Zacg~Zu3-.";
+// below secret is expired
+export const clientSecret = "-jrE4_iVxYX6AjTQu-N01b_6Zacg~Zu3-.";
---
+++ packages/cli/src/commonlib/testPass2.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+"use strict";
+
+export const secretId = "47cfa077-0454-48e9-b53e-c5cc7c86ba77";
+// below secret is expired
+export const secretValue = "-jrE4_iVxYX6AjTQu-N01b_6Zacg~Zu3-.";
